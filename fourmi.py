31f2bc8a0c9741d1c8a8d60da4433d85cc268495
P0l0863 <95549032+P0l0863@users.noreply.github.com>
2024-02-14 14:21:48 +0100

    passage/evaporation

---
+++ fourmi.py
@@ -0,0 +1,8 @@
+from typing import List, Tuple
+from graphe import *
+
+class Fourmi :
+    position : Noeud
+
+    def __init__(self, position):
+        self.position = position
--- luque-schultz-tp2.py
+++ luque-schultz-tp2.py
@@ -8,6 +8,18 @@ from graphe import *
 
 if __name__ == "main":
 	noeuds = [Noeud("A"), Noeud("B"), Noeud("C"), Noeud("D"), Noeud("E"), Noeud("F"), Noeud("G")]
-	
+
 
 	main(8)
+
+
+def evaporation(liens):
+    for i in liens:
+        i.pheromone = i.pheromone * 0.7
+
+
+def passage(lien):
+    lien.pheromone = lien.phermomone + (1 / lien.distance * Graphe.a) # TODO graphe a remettre en variable
+
+
+
