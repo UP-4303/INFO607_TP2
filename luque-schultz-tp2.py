959979d107558e682b49ae082286c68816b7f06e
P0l0863 <95549032+P0l0863@users.noreply.github.com>
2024-02-14 14:31:56 +0100

    evaporation+passage

--- luque-schultz-tp2.py
+++ luque-schultz-tp2.py
@@ -13,7 +13,7 @@ if __name__ == "main":
 	liens.append(noeuds[1].Connecte(noeuds[3], 1))
 	liens.append(noeuds[1].Connecte(noeuds[4], 3))
 	liens.append(noeuds[1].Connecte(noeuds[6], 4))
-	
+
 	liens.append(noeuds[2].Connecte(noeuds[4], 1))
 	liens.append(noeuds[2].Connecte(noeuds[5], 3))
 
@@ -29,4 +29,17 @@ if __name__ == "main":
 	# Chemin le plus court de A à G : A->B->C->E->F->G (distance : 7) ou A->B->E->F->G (distance : 7)
 	# Chemin le plus court pour le voyageur de commerce en partant de A : A->B->D->G->F->E->C (distance : 10)
 
-	graphe: Graphe = Graphe(noeuds, liens)
+    graphe: Graphe = Graphe(noeuds, liens)
+
+
+
+def evaporation(liens):
+    for i in liens:
+        #Evaporation progressive des phéromones (ne s'effaceront jamais completement mais deviennent négligeable face aux chemins régulièrement empruntés).
+        i.pheromone = i.pheromone * 0.7
+
+
+def passage(lien):
+    #Sur un même temps les chemins plus court seront plus parcourus, on laisse donc plus de phéromone si le chemin est plus court.
+    lien.pheromone = lien.phermomone + (1 / (lien.distance * Graphe.a)) # TODO graphe a remettre en variable
+
