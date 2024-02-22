af8530e170f845e12e84e35a56854f5f8e0528ac
P0l0863 <95549032+P0l0863@users.noreply.github.com>
2024-02-22 09:04:16 +0100

    le graphe

--- noeud.py
+++ noeud.py
@@ -7,17 +7,21 @@ class Noeud:
 	x:float
 	y:float
 
-	def __init__(self, label: str):
+	def __init__(self, label: str, index, nbPoint):
 		self.label = label
 		self.connexions = []
-		self.x = 0
-		self.y = 0
+		coord(index, nbPoint)
 
 	def Connecte(self, other, distance: int = 1, maxPheromones: float = 10): # TODO type hint
 		lien = Lien(self,other, distance, maxPheromones= maxPheromones)
 		self.connexions.append(lien)
 		other.connexions.append(lien)
 		return lien
+
+	def coord(self, index, nbPoint):
+		angleR = npy.radians((360 / nbPoint)*index)
+		self.x = 2* npy.cos(angleR)
+		self.y = 2* npy.sin(angleR)
 
 # Formate une liste de noeuds comme un chemin, en string
 def strChemin(noeuds: List[Noeud]):
